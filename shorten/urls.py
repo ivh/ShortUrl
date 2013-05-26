13357306ba9588fd30e1dfce2bca60fe0bc025d3
Thomas Marquart <thomas@marquart.se>
2013-05-26 13:10:43 +0200

    make app work in latest django, remove deprecated things

--- shorten/urls.py
+++ shorten/urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls.defaults import *
-
+from django.conf.urls import *
+from django.views.generic import DetailView
 from models import URL
+
+
+class UrlDetailView(DetailView):
+    model = URL
+
 
 urlpatterns = patterns('',
     url(r'^shorten/$','shorten.views.new',name='new'),
     url(r'^api/$','shorten.views.api',name='api'),
-    url(r'^(?P<object_id>\w+)/view/$', 'django.views.generic.list_detail.object_detail', {'queryset': URL.objects.all(),'template_name':'view.html'}),
+    url(r'^(?P<pk>\w+)/view/$', UrlDetailView.as_view(template_name="view.html")),
     url(r'^^(?P<key>\w+)/qrs/$','shorten.views.qrs',name='qrs'),
     url(r'^^(?P<key>\w+)/qrl/$','shorten.views.qrl',name='qrl'),
-    url(r'^(?P<key>\w+)/$', 'shorten.views.redir',name='redirect'),
+    url(r'^(?P<key>\w+)(.html)?$', 'shorten.views.redir',name='redirect'),
 )
